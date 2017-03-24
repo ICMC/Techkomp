0e0836b9dbe898245a043d0cc730de687f8513f5
Agnieszka Maule <Agnieszka Maule>
2017-03-24 07:25:45 -0300

    django installed

---
+++ django/myvenv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\profa\documents\anna\usp\techkomp\django\myvenv\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
