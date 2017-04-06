f0650ff3492ecc92d4bd9e1b15a7abcef6bb5ec2
Agnieszka Maule <anna.maulle@gmail.com>
2017-04-06 10:19:51 -0300

    learning how to use django

---
+++ django/myvenv/Scripts/blog/models.py
@@ -0,0 +1,24 @@
+from django.db import models
+
+# Create your models here.
+from django.db import models
+from django.utils import timezone
+
+# Django documentation for more Model fields and how to define things
+# https://docs.djangoproject.com/en/1.10/ref/models/fields/#field-types
+
+class Post(models.Model):
+    author = models.ForeignKey('auth.User')
+    title = models.CharField(max_length=200)
+    text = models.TextField()
+    created_date = models.DateTimeField(
+            default=timezone.now)
+    published_date = models.DateTimeField(
+            blank=True, null=True)
+
+    def publish(self):
+        self.published_date = timezone.now()
+        self.save()
+
+    def __str__(self):
+        return self.title
--- django/myvenv/Scripts/techkomp/settings.py
+++ django/myvenv/Scripts/techkomp/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '-rn3)3b#4n@@@&^@^7*ew^n%ycv4ffzww7h$+60z_bqkeoy0u&'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com']
 
 
 # Application definition
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'blog',
 ]
 
 MIDDLEWARE = [
@@ -118,3 +119,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
